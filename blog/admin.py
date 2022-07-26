6bdb9bdc1942a63a3e8f7024662338326127326d
Vahid Hasanzade <vahidzhe@gmail.com>
2022-07-26 16:45:20 +0400

    Meqale modelinin admin panel redakte edildi

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,21 @@
 from django.contrib import admin
-from blog.models import KateqoriyaModel
+from blog.models import KateqoriyaModel,MeqaleModel
 # Register your models here.
 
 admin.site.register(KateqoriyaModel)
+
+
+
+
+
+class MeqaleAdmin(admin.ModelAdmin):
+    search_fields = ('basliq','yazi')
+    list_display = (
+        'basliq',
+        # 'kateqoriya',
+        'yazar',
+        'slug',
+        'yaranma_tarixi',
+        'redakte_tarixi'
+    )
+admin.site.register(MeqaleModel,MeqaleAdmin)
--- blog/models/meqale.py
+++ blog/models/meqale.py
@@ -2,12 +2,13 @@ from django.db import models
 from blog.models import KateqoriyaModel
 from django.contrib.auth.models import User
 from autoslug import AutoSlugField
+from ckeditor.fields import RichTextField
 
 
 class MeqaleModel(models.Model):
     basliq = models.CharField(max_length=50)
     kateqoriya = models.ManyToManyField(KateqoriyaModel, related_name='meqale')
-    yazi = models.TextField()
+    yazi = RichTextField()
     sekil = models.ImageField(upload_to='meqale_sekilleri')
     yazar = models.ForeignKey(User, on_delete=models.CASCADE,
                               related_name='meqaleler')
