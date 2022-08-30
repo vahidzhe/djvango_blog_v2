f6fb08f5fec8efb72ba793ab901637db1b98fe65
Vahid Hasanzade <vahidzhe@gmail.com>
2022-08-30 21:50:37 +0400

    messages elave edildi

--- blog/views/serh_sil.py
+++ blog/views/serh_sil.py
@@ -2,6 +2,7 @@ import re
 from django.shortcuts import redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from blog.models import SerhModel
+from django.contrib import messages
 
 login_required(login_url='/')
 
@@ -11,6 +12,7 @@ def serh_sil(request, id):
 
     if request.user==serh.yazar or request.user==serh.meqale.yazar:
         serh.delete()
+        messages.success(request,'Şərh silindi.')
         return redirect('detay',slug=serh.meqale.slug)
 
 
