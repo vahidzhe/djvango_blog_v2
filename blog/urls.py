f5b1c3644634ea40d2a6163ef22100cfb0581d40
vahidzhe <vahidzhe@gmail.com>
2022-09-05 08:24:59 +0400

    hakkimda elave olundu

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from .views import anasehife, elaqe, kateqoriya, meqalelerim, detay, meqale_yaz, meqale_redakte,meqale_sil,serh_sil
-
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('', anasehife, name='anasehife'),
@@ -11,7 +11,10 @@ urlpatterns = [
     path('meqale_yaz', meqale_yaz, name='meqale_yaz'),
     path('meqale_redakte/<slug:slug>', meqale_redakte, name='meqale_redakte'),
     path('meqale_sil/<slug:slug>', meqale_sil, name='meqale_sil'),
-    path('serh_sil/<int:id>',serh_sil,name='serh_sil')
+    path('serh_sil/<int:id>',serh_sil,name='serh_sil'),
+    path('hakkimda', TemplateView.as_view(
+        template_name = 'pages/hakkimda.html'
+    ), name='hakkimda'),
 
 
 ]
