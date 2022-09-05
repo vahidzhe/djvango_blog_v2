0f7c4c42a8ed00baad18755f906a9e7daf85f451
vahidzhe <vahidzhe@gmail.com>
2022-09-05 12:12:42 +0400

    update secret key

--- blog/forms/elaqe_form.py
+++ blog/forms/elaqe_form.py
@@ -2,9 +2,15 @@ from dataclasses import fields
 from pyexpat import model
 from django import forms
 from blog.models import ElaqeModel
+from django.core.mail import send_mail
 
 
 class ElaqeForm(forms.ModelForm):
     class Meta:
         model = ElaqeModel
         fields = ('ad_soyad','email','yazi')
+
+    def email_gonder(self,mesaj):
+        send_mail(
+            
+        )
